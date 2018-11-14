e148d4fe62951b83c855fe94bd63d4f662232f6c
Matthew Carson <matthew24carson@gmail.com>
2018-11-13 21:09:03 -0500

    Over complicate styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path    				= require('path');
 const webpack 				= require('webpack');
 const { VueLoaderPlugin } 	= require('vue-loader');
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: {
 		app: [
-			'./src/app.js'
+			'./src/js/app.js',
+			'./src/scss/styles.scss'
 		]
 	},
 	output: {
@@ -23,6 +25,20 @@ module.exports = {
 				test: /\.js$/, 
 				exclude: /node_modules/, 
 				loader: "babel-loader" 
+			},
+			{
+				test: /\.[s?]css/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{ 
+						loader: 'css-loader', 
+						options: { 
+							import: false,
+							importLoaders: 1 
+						} 
+					},
+					'sass-loader'
+				]
 			}
 		]
 	},
@@ -32,6 +48,9 @@ module.exports = {
 		}
 	},
 	plugins: [
+		new MiniCssExtractPlugin({
+	    	filename: 'public/css/styles.css'
+	    }),
 		new VueLoaderPlugin()
 	]
 }
